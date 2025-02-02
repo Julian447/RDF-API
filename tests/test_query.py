5d71c961b5157b6dd2d13fcd8122d5a7c72b237d
Nistec <julian.larsen07@gmail.com>
2025-02-02 17:57:43 +0100

    beginning of test query

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,12 +1,15 @@
 from rdf_api.create_query import process_new_nodes,  check_graph_exist
 from rdf_api.datastructure.triple_structure import TripleList, Triple
+from rdf_api.datastructure.query_structure import Query
+from rdf_api.read_query import get_graph, run_query
 
-from rdflib.namespace import (FOAF, RDF, RDFS)
+from rdflib.namespace import (FOAF, RDF, RDFS, XSD)
 import pytest
 from pytest import FixtureRequest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from main import app
+
 
 graph_name = "test"
 graph_path = f'file://{graph_name}/' 
@@ -18,7 +21,17 @@ t1= Triple(
     obj= f'{graph_path1}test1',
     obj_is_literal=None
 )
-# assert t1.sub ==  
+t1_read = Query(
+query = """
+    PREFIX t1: <t1_path>
+    PREFIX t2: <t2_path> 
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
+
+    SELECT * WHERE {
+        t1:test a t2:test1 
+    }
+    """.replace("t1_path", graph_path).replace("t2_path", graph_path1)
+)
 
 t2= Triple(
     sub = f'{graph_path}test2',
@@ -27,13 +40,35 @@ t2= Triple(
     obj= f'{graph_path}test2',
     obj_is_literal=None
 )
+t2_read = Query(
+query = """
+    PREFIX t1: <t1_path>
+    PREFIX t2: <t2_path> 
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
+
+    SELECT * WHERE {
+        t1:test2 a t1:test2 
+    }
+    """.replace("t1_path", graph_path).replace("t2_path", graph_path1)
+)
 
 t3= Triple(
     sub = f"{graph_path}test",
     sub_is_literal=None,
     pred=FOAF.name,
     obj= f"{graph_path1}testname",
-    obj_is_literal=None
+    obj_is_literal=XSD.string
+)
+t3_read = Query(
+query = """
+    PREFIX t1: <t1_path>
+    PREFIX t2: <t2_path> 
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
+
+    SELECT * WHERE {
+        t1:test xsd:string t2:testname 
+    }
+    """.replace("t1_path", graph_path).replace("t2_path", graph_path1)
 )
 
 t : list[Triple] = [t1,t2,t3]
@@ -83,6 +118,26 @@ def test_API_create_node_query():
     assert r.status_code == 200
     assert r.json() == {"success": True}
 
+def test_API_read_query():
+
+    json = triples.model_dump_json()
+
+    # r_fail = client.post("/test/create_item/tests", content=json)
+    #
+    # assert r_fail.status_code != 200
+    #
+    token = get_token()
+
+    r = client.post("/test/create_item/tests", content=json, 
+                      headers= {
+                      "Authorization": f"Bearer {token}"
+                      })
+    res = run_query(graphname="test",query=t1_read,graph_path="tests")
+    print(res)
+    # assert 
+    # assert r.status_code == 200
+    # assert r.json() == {"success": True}
+test_API_read_query()
 
 def test_graph_exists():
     graph = check_graph_exist(graph_name, "tests")
